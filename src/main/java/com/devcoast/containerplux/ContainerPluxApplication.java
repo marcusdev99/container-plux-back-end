bcaa09faf502ee17f18cb593a4cd58b5ae9364b5
marcusdev99 <marcus.sea13@gmail.com>
2023-08-20 21:50:31 -0600

    Initial commit for container plux back end application.

---
+++ src/main/java/com/devcoast/containerplux/ContainerPluxApplication.java
@@ -0,0 +1,13 @@
+package com.devcoast.containerplux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerPluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerPluxApplication.class, args);
+	}
+
+}
