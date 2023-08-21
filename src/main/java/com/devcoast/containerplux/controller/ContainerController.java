6062e3d370af0fda73db82043ea4e89b974042d2
marcusdev99 <marcus.sea13@gmail.com>
2023-08-21 17:59:50 -0600

    feat : addition of POST method to save containers

--- src/main/java/com/devcoast/containerplux/controller/ContainerController.java
+++ src/main/java/com/devcoast/containerplux/controller/ContainerController.java
@@ -4,9 +4,7 @@ import com.devcoast.containerplux.model.Container;
 import com.devcoast.containerplux.repository.ContainerRepository;
 import com.devcoast.containerplux.service.ContainerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,10 @@ public class ContainerController {
         return containerService.getAllContainers();
     }
 
+    @PostMapping("/containers")
+    public Container saveContainer(@RequestBody Container container){
+        containerService.saveContainer(container);
+        return container;
+    }
 
 }
