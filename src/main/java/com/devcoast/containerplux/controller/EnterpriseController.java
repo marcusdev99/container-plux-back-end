6c5799395e011fbc1baf884c76a409d6b2113be2
marcusdev99 <marcus.sea13@gmail.com>
2023-09-05 16:32:30 -0600

    feat : addition of enterprise controller class
    methods declared:
    -getAllEnterprises -> GET

---
+++ src/main/java/com/devcoast/containerplux/controller/EnterpriseController.java
@@ -0,0 +1,22 @@
+package com.devcoast.containerplux.controller;
+
+import com.devcoast.containerplux.model.Enterprise;
+import com.devcoast.containerplux.service.EnterpriseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@CrossOrigin
+@Controller
+public class EnterpriseController {
+    @Autowired
+    EnterpriseService enterpriseService;
+
+    @GetMapping("/enterprises")
+    public List<Enterprise> getAllEnterprises(){
+        return enterpriseService.getAllEnterprises();
+    }
+}
