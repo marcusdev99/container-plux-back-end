285b77ff9ef4781bc667d1769f95bf7f6c9a38ca
marcusdev99 <marcus.sea13@gmail.com>
2023-09-05 16:40:02 -0600

    refact : addition of repository annotation

--- src/main/java/com/devcoast/containerplux/repository/EnterpriseRepository.java
+++ src/main/java/com/devcoast/containerplux/repository/EnterpriseRepository.java
@@ -2,6 +2,8 @@ package com.devcoast.containerplux.repository;
 
 import com.devcoast.containerplux.model.Enterprise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EnterpriseRepository extends JpaRepository<Enterprise,Long> {
 }
